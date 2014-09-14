c193b9b58a8f2d34d9bcd686ad4a14f3d9c79ade
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2014-09-14 17:54:57 +0100

    second line falling animation added

--- AndroidAnimTranslate/src/com/exercise/AndroidAnimTranslate/AndroidAnimTranslateActivity.java
+++ AndroidAnimTranslate/src/com/exercise/AndroidAnimTranslate/AndroidAnimTranslateActivity.java
@@ -1,7 +1,5 @@
 package com.exercise.AndroidAnimTranslate;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +8,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class AndroidAnimTranslateActivity extends Activity {
@@ -18,19 +17,23 @@ public class AndroidAnimTranslateActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        Button btnStartGame = (Button)findViewById(R.id.btnStartGame);
         final Animation animationFalling = AnimationUtils.loadAnimation(this, R.anim.falling);
         final AndroidAnimTranslateActivity app = this;
-        final ArrayList<ImageView> images = new ArrayList<ImageView>();
+
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.llMain);
+        mainLayout.setOnClickListener(new Button.OnClickListener(){
+		    @Override
+		    public void onClick(View arg0) {
+		    }
+        });
         
+        
+        Button btnStartGame = (Button)findViewById(R.id.btnStartGame);
         btnStartGame.setOnClickListener(new Button.OnClickListener(){
 		    @Override
 		    public void onClick(View arg0) {
 		    	ImageView fi = new ImageView(app);
 		    	fi.setImageResource(R.drawable.ic_launcher);
-		    	
-		    	images.add(fi);
 		    	
 		    	RelativeLayout rl = (RelativeLayout) findViewById(R.id.rlLine1);
 		    	
@@ -41,10 +44,38 @@ public class AndroidAnimTranslateActivity extends Activity {
 		    	
 		    	lp.addRule(RelativeLayout.ALIGN_LEFT);
 		    	
-		    	rl.addView(images.get(images.size()-1), lp);
-	    		for (ImageView imageView : images) {
-	    			imageView.startAnimation(animationFalling);
-				}
+		    	rl.addView(fi, lp);
+		    	fi.startAnimation(animationFalling);
+		    	
+		    	ImageView fi2 = new ImageView(app);
+		    	fi2.setImageResource(R.drawable.ic_launcher);
+		    	
+		    	RelativeLayout rl2 = (RelativeLayout) findViewById(R.id.rlLine2);
+		    	
+		    	RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(
+	    			LayoutParams.WRAP_CONTENT,
+	    		    LayoutParams.WRAP_CONTENT
+		    	);
+		    	
+		    	lp2.addRule(RelativeLayout.ALIGN_LEFT);
+		    	
+		    	rl2.addView(fi2, lp2);
+		    	fi2.startAnimation(animationFalling);
+
+		    	ImageView fi3 = new ImageView(app);
+		    	fi3.setImageResource(R.drawable.ic_launcher);
+		    	
+		    	RelativeLayout.LayoutParams lp3 = new RelativeLayout.LayoutParams(
+	    			LayoutParams.WRAP_CONTENT,
+	    		    LayoutParams.WRAP_CONTENT
+		    	);
+		    	
+		    	lp3.addRule(RelativeLayout.ALIGN_LEFT);
+		    	
+		    	rl.addView(fi3, lp3);
+		    	
+		    	fi3.startAnimation(animationFalling);
+		    	animationFalling.setStartOffset(1000);
 		    }
         });
     }
