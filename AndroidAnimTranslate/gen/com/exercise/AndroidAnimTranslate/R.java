cf79df107a5c6cce06b6d7e6e9f382bb365d975a
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2014-09-14 15:27:27 +0100

    btnStartGame added

--- AndroidAnimTranslate/gen/com/exercise/AndroidAnimTranslate/R.java
+++ AndroidAnimTranslate/gen/com/exercise/AndroidAnimTranslate/R.java
@@ -17,10 +17,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btnStartGame=0x7f060005;
         public static final int imageView1=0x7f060000;
         public static final int imageView2=0x7f060001;
         public static final int imageView3=0x7f060002;
         public static final int imageView4=0x7f060003;
+        public static final int llMenu=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -28,5 +30,6 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
+        public static final int onStartGameClick=0x7f050002;
     }
 }
