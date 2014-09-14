77590af56b6c5566bc21aab0795f9c58fbe2ab4c
Kristian Lubenov <kristian.lubenov@gmail.com>
2014-09-14 21:40:15 +0300

    Revert "* Movement updates"
    This reverts commit c69ca317109011e1dc30c33b87ebed7a3660c83f.

--- AndroidAnimTranslate/src/com/exercise/AndroidAnimTranslate/AndroidAnimTranslateActivity.java
+++ AndroidAnimTranslate/src/com/exercise/AndroidAnimTranslate/AndroidAnimTranslateActivity.java
@@ -1,7 +1,5 @@
 package com.exercise.AndroidAnimTranslate;
 
-import java.util.Random;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,8 +18,6 @@ public class AndroidAnimTranslateActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         final Animation animationFalling = AnimationUtils.loadAnimation(this, R.anim.falling);
-        final Animation animationLine1 = AnimationUtils.loadAnimation(this, R.anim.falling);
-        final Animation animationLine2 = AnimationUtils.loadAnimation(this, R.anim.falling);
         final AndroidAnimTranslateActivity app = this;
 
         LinearLayout mainLayout = (LinearLayout) findViewById(R.id.llMain);
@@ -36,7 +32,7 @@ public class AndroidAnimTranslateActivity extends Activity {
         btnStartGame.setOnClickListener(new Button.OnClickListener(){
 		    @Override
 		    public void onClick(View arg0) {
-		    	final ImageView fi = new ImageView(app);
+		    	ImageView fi = new ImageView(app);
 		    	fi.setImageResource(R.drawable.ic_launcher);
 		    	
 		    	RelativeLayout rl = (RelativeLayout) findViewById(R.id.rlLine1);
@@ -51,7 +47,7 @@ public class AndroidAnimTranslateActivity extends Activity {
 		    	rl.addView(fi, lp);
 		    	fi.startAnimation(animationFalling);
 		    	
-		    	final ImageView fi2 = new ImageView(app);
+		    	ImageView fi2 = new ImageView(app);
 		    	fi2.setImageResource(R.drawable.ic_launcher);
 		    	
 		    	RelativeLayout rl2 = (RelativeLayout) findViewById(R.id.rlLine2);
@@ -64,77 +60,22 @@ public class AndroidAnimTranslateActivity extends Activity {
 		    	lp2.addRule(RelativeLayout.ALIGN_LEFT);
 		    	
 		    	rl2.addView(fi2, lp2);
-		    	fi2.startAnimation(animationLine1);
-		    	
-		    	final ImageView fi3 = new ImageView(app);
+		    	fi2.startAnimation(animationFalling);
+
+		    	ImageView fi3 = new ImageView(app);
 		    	fi3.setImageResource(R.drawable.ic_launcher);
 		    	
-		    	RelativeLayout rl3 = (RelativeLayout) findViewById(R.id.rlLine3);
-		    	
-		    	RelativeLayout.LayoutParams lp1 = new RelativeLayout.LayoutParams(
+		    	RelativeLayout.LayoutParams lp3 = new RelativeLayout.LayoutParams(
 	    			LayoutParams.WRAP_CONTENT,
 	    		    LayoutParams.WRAP_CONTENT
 		    	);
 		    	
-		    	lp1.addRule(RelativeLayout.ALIGN_LEFT);
+		    	lp3.addRule(RelativeLayout.ALIGN_LEFT);
 		    	
-		    	rl3.addView(fi3, lp1);
-		    	fi3.startAnimation(animationLine2);
+		    	rl.addView(fi3, lp3);
 		    	
-		    	final Random rnd = new Random();
-		    	int rnd1 = (int)(rnd.nextInt(10000));
-		    	int rnd2 = (int)(rnd.nextInt(10000));
-		    	int rnd3 = (int)(rnd.nextInt(10000));
-		    	
-		    	animationFalling.setStartOffset(rnd1+3);
-		    	animationLine1.setStartOffset(rnd2+3);
-		    	animationLine2.setStartOffset(rnd3+3);
-		    	
-		    	animationFalling.setAnimationListener(new Animation.AnimationListener(){
-		    	    @Override
-		    	    public void onAnimationStart(Animation arg0) {
-		    	    }           
-		    	    @Override
-		    	    public void onAnimationRepeat(Animation arg0) {
-		    	    }           
-		    	    @Override
-		    	    public void onAnimationEnd(Animation arg0) {
-				    	int rnd1 = (int)(rnd.nextFloat()*1000);
-				    	animationFalling.setStartOffset(rnd1+10);
-				    	fi.startAnimation(animationFalling);
-		    	    }
-		    	});
-		    	
-		    	
-		    	animationLine1.setAnimationListener(new Animation.AnimationListener(){
-		    	    @Override
-		    	    public void onAnimationStart(Animation arg0) {
-		    	    }           
-		    	    @Override
-		    	    public void onAnimationRepeat(Animation arg0) {
-		    	    }           
-		    	    @Override
-		    	    public void onAnimationEnd(Animation arg0) {
-				    	int rnd2 = (int)(rnd.nextFloat()*1000);
-				    	animationLine1.setStartOffset(rnd2+10);
-				    	fi2.startAnimation(animationLine1);
-		    	    }
-		    	});
-		    	
-		    	animationLine2.setAnimationListener(new Animation.AnimationListener(){
-		    	    @Override
-		    	    public void onAnimationStart(Animation arg0) {
-		    	    }           
-		    	    @Override
-		    	    public void onAnimationRepeat(Animation arg0) {
-		    	    }           
-		    	    @Override
-		    	    public void onAnimationEnd(Animation arg0) {
-				    	int rnd2 = (int)(rnd.nextFloat()*1000);
-				    	animationLine2.setStartOffset(rnd2+10);
-				    	fi3.startAnimation(animationLine2);
-		    	    }
-		    	});
+		    	fi3.startAnimation(animationFalling);
+		    	animationFalling.setStartOffset(1000);
 		    }
         });
     }
